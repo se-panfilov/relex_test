d104940c028f2790e057cb1aacd524186640d714
Sergey Panfilov <se-panfilov@ya.ru>
2017-01-22 12:41:02 +0100

    Added filtering feature

--- src/components/contacts_list.jsx
+++ src/components/contacts_list.jsx
@@ -12,14 +12,45 @@ export default class ContactsList extends React.Component {
     actions: PropTypes.object.isRequired
   };
 
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      filter: '',
+      displayed: props.contacts
+    };
+  }
+
+  onInputChange (e) {
+
+    const newDisplayed = this.props.contacts.filter(v => {
+      const firstName = v.firstName.toLowerCase();
+      const lastName = v.lastName.toLowerCase();
+      const val = e.target.value.toLowerCase();
+
+      return firstName.includes(val) || lastName.includes(val)
+    });
+
+    this.setState({
+      filter: '',
+      displayed: newDisplayed
+    })
+  };
+
   render () {
     const { contacts, dispatch, actions } = this.props;
     const { selectContact } = actions;
 
     return (
       <div className="contacts-list">
+        <input type="search"
+               placeholder="Search"
+               value={this.state.input}
+               onChange={
+                 this.onInputChange.bind(this)
+               }/>
         <ul className="contacts-list__list">
-          {contacts.map(item =>
+          {this.state.displayed.map(item =>
             <ContactsListItem
               key={item._id}
               id={item._id}
